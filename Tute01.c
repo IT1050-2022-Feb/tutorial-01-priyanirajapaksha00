b9b0f897d37b3d9d59986904dffb55bbef0c7fb4
it21174858 <99777444+IT21174858@users.noreply.github.com>
2022-03-09 17:15:02 +0000

    I did 1st exercise

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1, mark2;
+  int calculation;
+  
+  printf("Enter mark1:");
+  scanf("%d",&mark1);
+ 
+  printf("Enter mark2:");
+  scanf("%d",&mark2);
+
+  calculation=mark1+mark2;
+  printf("Sum is=%d",calculation);
   
   return 0;
 }
